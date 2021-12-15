f2807d4e66ef6193d0f2a255ed66ee020e18fa14
Gahasa Purba <gahasapurba.tech@gmail.com>
2021-12-15 10:45:17 +0700

    Add files via upload

---
+++ resources/views/pages/category/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('title', 'Tambah Data Kategori')
+
+@section('content')
+
+@php
+    use RealRashid\SweetAlert\Facades\Alert;
+    if($errors->any())
+    {
+        alert()->error('Kesalahan Pengisian Data','Silahkan Ulangi Mengisi Data');
+    }
+@endphp
+
+<div
+    class="section-content section-dashboard-home"
+    data-aos="fade-up"
+>
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+            <h2 class="dashboard-title">Tambah Kategori</h2>
+            <p class="dashboard-subtitle">
+                Silahkan Tambah Kategori Baru
+            </p>
+        </div>
+        <div class="dashboard-content">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{ route('category.store') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="name">Nama Kategori</label>
+                                            <input type="text" name="name" class="form-control @if($errors->has('name')) is-invalid @endif" placeholder="Masukkan nama kategori" value="{{ old('name') }}" autofocus>
+                                            @if($errors->has('name'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    @foreach ($errors->get('name') as $error)
+                                                        <strong>{{ $error }}</strong>
+                                                    @endforeach
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-right">
+                                        <button type="submit" class="btn btn-primary px-5">
+                                            Simpan
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/pages/category/index.blade.php
@@ -0,0 +1,151 @@
+@extends('layouts.app')
+
+@section('title', 'Manajemen Kategori')
+
+@section('content')
+
+<div
+    class="section-content section-dashboard-home"
+    data-aos="fade-up"
+>
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+            <h2 class="dashboard-title">Manajemen Kategori</h2>
+            <p class="dashboard-subtitle">
+                Manajemen Kategori Pembelian di SIP Del
+            </p>
+        </div>
+        <div class="dashboard-content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <a href="{{ route('category.create') }}" class="btn btn-primary mb-3">
+                                + Tambah Kategori Baru
+                            </a>
+                            <div class="table-responsive">
+                                <table class="table table-hover scroll-horizontal-vertical w-100" id="crudTable">
+                                    <thead>
+                                        <tr>
+                                            <th style="display: none">ID</th>
+                                            <th>No</th>
+                                            <th class="text-center">Nama Kategori</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@push('addon-script')
+
+    <script>
+        var datatable = $('#crudTable').DataTable({
+            lengthMenu: [[5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "All"]],
+            processing: true,
+            serverSide: true,
+            ordering: true,
+            ajax: {
+                url: '{!! url()->current() !!}',
+            },
+            columns: [
+                {
+                    data: 'id',
+                    visible: false,
+                    orderable: false,
+                    searchable: false,
+                    name: 'id',
+                    width: '5%',
+                    class: 'text-center',
+                },
+                {
+                    data: 'DT_RowIndex',
+                    name: 'DT_RowIndex',
+                    orderable: false,
+                    searchable: false,
+                    width: '5%',
+                    class: 'text-center',
+                },
+                { data: 'name', name: 'name' },
+                {
+                    data: 'action',
+                    name: 'action',
+                    orderable: false,
+                    searchable: false,
+                    width: '10%',
+                    class: 'text-center',
+                },
+            ],
+            // dom: 'Bfrtip',
+            // buttons: [
+            //     {
+            //         extend: 'copy',
+            //         exportOptions: {
+            //             columns: [1, 2]
+            //         }
+            //     },
+            //     {
+            //         extend: 'csv',
+            //         exportOptions: {
+            //             columns: [1, 2]
+            //         }
+            //     },
+            //     {
+            //         extend: 'excel',
+            //         exportOptions: {
+            //             columns: [1, 2]
+            //         }
+            //     },
+            //     {
+            //         extend: 'pdf',
+            //         exportOptions: {
+            //             columns: [1, 2]
+            //         }
+            //     },
+            //     {
+            //         extend: 'print',
+            //         exportOptions: {
+            //             columns: [1, 2]
+            //         }
+            //     },
+            // ],
+        })
+    </script>
+
+    <script type="text/javascript">
+        $("body").on("click",".remove-category",function(){
+            var current_object = $(this);
+            Swal.fire({
+                title: 'Apakah anda yakin ingin menghapus data ini?',
+                text: "Anda tidak akan bisa mengembalikan data ini setelah menghapusnya. Data pembelian yang terkait dengan kategori ini akan terdampak, dan tidak bisa ditampilkan sementara. Anda dapat merestore kategori ini nanti, atau ikut menghapus sementara data pembelian terkait",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Ya, Hapus'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    var action = current_object.attr('data-action');
+                    var token = jQuery('meta[name="csrf-token"]').attr('content');
+                    var id = current_object.attr('data-id');
+
+                    $('body').html("<form class='remove-form' method='post' action='"+action+"'></form>");
+                    $('body').find('.remove-form').append('<input name="_method" type="hidden" value="delete">');
+                    $('body').find('.remove-form').append('<input name="_token" type="hidden" value="'+token+'">');
+                    $('body').find('.remove-form').append('<input name="id" type="hidden" value="'+id+'">');
+                    $('body').find('.remove-form').submit();
+                }
+            });
+        });
+    </script>
+
+@endpush
