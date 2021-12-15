0c3f82c95dc29269c2778610f5fdd967c05afef4
Gahasa Purba <gahasapurba.tech@gmail.com>
2021-12-15 10:46:17 +0700

    Add files via upload

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:3|unique:categories,name',
+        ];
+    }
+}
